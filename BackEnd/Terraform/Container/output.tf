df4939b08f0fbe1e43b6237d1e863ba72d6758c2
louay kharouf <louay.khrouf@insat.ucar.tn>
2023-07-14 12:58:00 +0100

    first

---
+++ BackEnd/Terraform/Container/output.tf
@@ -0,0 +1,15 @@
+output "subscription_id" {
+  value = var.subscription_id
+}
+
+output "resource_group_name" {
+  value = var.resource_group_name
+}
+
+output "storage_account_name" {
+  value = var.storage_account_name
+}
+
+output "container_name" {
+  value = var.container_name
+}
