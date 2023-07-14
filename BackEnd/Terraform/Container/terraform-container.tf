b8033acf85a9752be44a2275097bb3d8da417c0e
louay kharouf <louay.khrouf@insat.ucar.tn>
2023-07-14 12:46:29 +0100

    first

--- BackEnd/Terraform/Container/terraform-container.tf
+++ BackEnd/Terraform/Container/terraform-container.tf
@@ -1,4 +1,4 @@
-/*terraform {
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -7,13 +7,18 @@
   }
   required_version = ">= 0.14.9"
 }
-*/
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
 }
+
 resource "azurerm_storage_container" "scriptscontainer" {
   name                  = var.container_name
   storage_account_name  = var.storage_account_name
   container_access_type = "blob"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
